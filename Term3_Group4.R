0fa449e6cdd64c177367de7b37c22cd1c3bc923b
paulforst <paulforst@gmail.com>
2017-11-08 11:10:37 -0500

    Initial R Script

---
+++ Term3_Group4.R
@@ -0,0 +1,6 @@
+############################################
+#Term 3 - Group 4 Group Project
+#Members: Tammy Hang, Jay Bektasevic, Andrew Brill, Paul Forst
+#
+#
+############################################
