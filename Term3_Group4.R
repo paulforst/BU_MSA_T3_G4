e64233a7213106cea1c700431aedc3c67acb6dca
paulforst <paulforst@gmail.com>
2017-11-08 21:00:02 -0500

    General layout of sections

--- Term3_Group4.R
+++ Term3_Group4.R
@@ -6,3 +6,36 @@
 #
 #Output:
 ############################################
+
+#Load Necessary Libraries
+library(tidyverse)
+library(tm)
+
+
+#Pull NY Times Articles
+
+
+#Pull BBC Articles
+
+
+#Pull Other Data
+
+
+#Create Corpus
+
+
+#Create Train, Test and Validation sets
+
+
+#Create Model
+#Test Model
+#Validate Model
+
+#Classification
+#Lexical Diversity/Reading Level
+#K-Folds
+#Latent Semantic Analysis (aka Latent Semantic Indexing)
+#Binary classification on multiple sources
+#Article filtering
+#Lexical diversity - Reading level
+#Optimization - penalty for misclassification
