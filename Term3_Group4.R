2db91e5166c9ed84d1941f4e7b646dd77d7f1321
paulforst <paulforst@gmail.com>
2017-11-16 12:41:19 -0500

    Fixing issues of prior commit

--- Term3_Group4.R
+++ Term3_Group4.R
@@ -15,7 +15,6 @@ if(length(new.packages)) install.packages(new.packages)
 #Load Neccessary Packages
 sapply(packages, require, character.only = TRUE)
 
-<<<<<<< HEAD
 #Options
 options(stringsAsFactors = FALSE)
 
@@ -31,10 +30,9 @@ for (i in 1:3){
         #Replace "7" with i to perform the loop for the desired months
         nyt_results <- fromJSON(paste0(nyt_url, 7, ".json?&api-key=", nyt_key))
 }
-=======
+
 #Pull NY Times Articles
 #https://developer.nytimes.com/
->>>>>>> d7f58b3167bff1cd0a7afb873d76401a598cbd9e
 
 #Pull Guardian Data
 #https://bonobo.capi.gutools.co.uk/register/developer
@@ -42,24 +40,12 @@ for (i in 1:3){
 #Create Corpus
 
 #Write metadata of articles to database
-<<<<<<< HEAD
-=======
-#DB connection information
-db_host <- "bu-iaa-db.cjoepp1m87gp.us-east-2.rds.amazonaws.com"
-db_user <- 
-db_password <- 
-db_name <- "pforst"
-
->>>>>>> d7f58b3167bff1cd0a7afb873d76401a598cbd9e
 #Database connection
 mydb = dbConnect(MySQL(), user=db_user, password=db_password, dbname= db_name, host= db_host)
 
 #Write information to table
-<<<<<<< HEAD
 dbWriteTable(mydb, "nytimes", df2, append = 'FALSE', row.names = FALSE)
-=======
 dbWriteTable(mydb, "articles", df2, append = 'FALSE', row.names = FALSE)
->>>>>>> d7f58b3167bff1cd0a7afb873d76401a598cbd9e
 
 #Create Train, Test and Validation sets
 
