fc1854cea6f74469417e090a6fe1d5b85321408f
Jay_Bektasevic <jbektasevic@bellarmine.edu>
2017-12-07 09:49:42 -0500

    Added a spelling option in clean.corpus() function and moved asynchronous function removeSpecialChars() inside the clean.corpus function.

--- corpus_functions.R
+++ corpus_functions.R
@@ -15,7 +15,7 @@
 #   Load Required Packages and Files  
 
 #   Check that necessary packages are installed
-    packages <- c("SnowballC","tm")
+    packages <- c("SnowballC","tm", "hunspell")
     new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
     if(length(new.packages)) install.packages(new.packages)
 
@@ -30,17 +30,15 @@
 #   stemming is a boolean to know if stemming should be performed
 #   Outputs:
 #   cleaned corpus of the orginal text
-    
-    removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]","",x)
 
-    clean.corpus <- function(x, add_stopwords = NULL, stemming = FALSE) {
+    clean.corpus <- function(x, add_stopwords = NULL, stemming = FALSE, spelling = FALSE) {
     
 #       Creating corpus from vector
         corpus <- Corpus(VectorSource(x))
         corpus <- tm_map(corpus,  removePunctuation)
         corpus <- tm_map(corpus,  stripWhitespace)
         corpus <- tm_map(corpus,  removeNumbers)
-        corpus <- tm_map(corpus, removeSpecialChars)
+        corpus <- tm_map(corpus, function(x) gsub("[^a-zA-Z0-9 ]","",x))
         corpus <- tm_map(corpus, tolower)
 #       Remove standard stopwords
         if(is.null(add_stopwords)){
@@ -53,9 +51,13 @@
         if(stemming){
             corpus <- tm_map(corpus, stemDocument)
         }
+#       Removes misspelled (non-English) words        
+        if(spelling){
+            corpus <- tm_map(corpus, removeWords, sort(unique(unlist(hunspell_find(as.character(corpus)))))) 
+        }                             
 #       Ensures that the corpus is returned in a plain text, otherwise, depending on the version of 
 #       "tm" package it can cause issues with the creation of Document-Term Matrix.  
-        corpus <- tm_map(corpus, PlainTextDocument)
+        corpus1 <- tm_map(corpus, PlainTextDocument)
         return(corpus)
     }
 
