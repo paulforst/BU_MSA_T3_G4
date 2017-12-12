29ba36fe6a3607ce0f9d8a49b3a9f4a276778816
Tammy Hang <thang@bellarmine.edu>
2017-12-12 12:11:26 -0500

    Updated Topic Model for NYT and Guardian

--- TopicModeling.R
+++ TopicModeling.R
@@ -13,33 +13,94 @@
 #   ____________________________________________________________________________
 #   Document Term Matrix was transformed to dataframe                       ####
 
+    #Topic Model for Combined Data
+    
 #   renaming tdm 
     dtm <- tdm
     dtm
 
 #   LDA function from topic model package k set to 4 for 4- topic LDA model 
-    nytimes_lda <- LDA(dtm, k = 4, control = list(seed = 1234))
-    nytimes_lda
+    all_lda <- LDA(dtm, k = 4, control = list(seed = 1234))
+    all_lda
 
 #   word topic probabilities 
-    nytimes_topics <- tidy(nytimes_lda, matrix = "beta")
-    nytimes_topics
+    all_topics <- tidy(all_lda, matrix = "beta")
+    all_topics
 
 #   Visualization of 4 topics that were extracted 
-    nytimes_top_terms <- nytimes_topics %>%
+    all_top_terms <- all_topics %>%
                   group_by(topic) %>%
-                  top_n(20, beta) %>%
+                  top_n(10, beta) %>%
                   ungroup() %>%
                   arrange(topic, -beta)
 
-    nytimes_top_terms %>%
+    all_top_terms %>%
                   mutate(term = reorder(term, beta)) %>%
                   ggplot(aes(term, beta, fill = factor(topic))) +
                   geom_col(show.legend = FALSE) +
                   facet_wrap(~ topic, scales = "free") +
                   coord_flip() + ggtitle('Top terms in each LDA topic')
 
+   
     
+    #------------------------------------------------------
+    #Topic Model for New York Times
+    #   renaming tdm 
+    dtm <- dtm_nyt
+    dtm
+    
+    #   LDA function from topic model package k set to 4 for 4- topic LDA model 
+    nytimes_lda <- LDA(dtm, k = 4, control = list(seed = 1234))
+    nytimes_lda
+    
+    #   word topic probabilities 
+    nytimes_topics <- tidy(nytimes_lda, matrix = "beta")
+    nytimes_topics
+    
+    #   Visualization of 4 topics that were extracted 
+    nytimes_top_terms <- nytimes_topics %>%
+        group_by(topic) %>%
+        top_n(10, beta) %>%
+        ungroup() %>%
+        arrange(topic, -beta)
+    
+    nytimes_top_terms %>%
+        mutate(term = reorder(term, beta)) %>%
+        ggplot(aes(term, beta, fill = factor(topic))) +
+        geom_col(show.legend = FALSE) +
+        facet_wrap(~ topic, scales = "free") +
+        coord_flip() + ggtitle('NYT Top terms in each LDA topic')
+    
+#------------------------------------------------------    
+    #Topic Model for Guardian
+    #   renaming tdm 
+    dtm <- dtm_guardian
+    dtm
+    
+    #   LDA function from topic model package k set to 4 for 4- topic LDA model 
+    guardian_lda <- LDA(dtm, k = 4, control = list(seed = 1234))
+    guardian_lda
+    
+    #   word topic probabilities 
+    guardian_topics <- tidy(guardian_lda, matrix = "beta")
+    guardian_topics
+    
+    #   Visualization of 4 topics that were extracted 
+    guardian_top_terms <- guardian_topics %>%
+        group_by(topic) %>%
+        top_n(10, beta) %>%
+        ungroup() %>%
+        arrange(topic, -beta)
+    
+    guardian_top_terms %>%
+        mutate(term = reorder(term, beta)) %>%
+        ggplot(aes(term, beta, fill = factor(topic))) +
+        geom_col(show.legend = FALSE) +
+        facet_wrap(~ topic, scales = "free") +
+        coord_flip() + ggtitle('Guardian Top terms in each LDA topic')
+    
+    
+     
     
     corp_guardian <- clean.corpus(combined_data[combined_data$source == "Guardian"]$body, stemming = TRUE)
     
